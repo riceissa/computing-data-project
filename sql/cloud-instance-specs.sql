0f18c56ceeb774f6c7d80106cd9c7a2a1ff16418
Issa Rice <riceissa@gmail.com>
2017-11-18 19:42:54 -0800

    add more spec params

--- sql/cloud-instance-specs.sql
+++ sql/cloud-instance-specs.sql
@@ -3,5 +3,9 @@ create table cloud_instance_specs (
     name varchar(100) not null,
     ram float(6,2),
     cpu float(6,2),
+    ecu float(6,2),
+    processor varchar(100),
+    network_throughput float(6,2),
+    storage varchar(100),
     unique key name(`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;

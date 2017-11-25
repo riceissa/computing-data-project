ad7fc5dce7d3c14b3880fd7912e0f7ec53841a62
Issa Rice <riceissa@gmail.com>
2017-11-25 15:01:52 -0800

    add units

--- sql/cloud-instance-costs.sql
+++ sql/cloud-instance-costs.sql
@@ -1,7 +1,10 @@
 create table cloud_instance_costs (
     cloud_instance_cost_id int(11) not null auto_increment primary key,
     name varchar(100) not null,
+
+    # Cost per hour
     cost float(7,5),
+
     odate date,
     region varchar(100),
     operating_system varchar(100),

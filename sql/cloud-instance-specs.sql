971bbbaa267931c6abafec7e0abc2ee6ce9881cf
Issa Rice <riceissa@gmail.com>
2017-11-25 14:34:26 -0800

    Mention units for RAM

--- sql/cloud-instance-specs.sql
+++ sql/cloud-instance-specs.sql
@@ -1,7 +1,11 @@
 create table cloud_instance_specs (
     cloud_instance_spec_id int(11) not null auto_increment primary key,
     name varchar(100) not null,
+
+    # Measured in "GB", which could be gigabytes or gibibytes. Some pricing
+    # pages don't make it clear which one is being used.
     ram float(6,2),
+
     cpu float(6,2),
     ecu float(6,2),
     processor varchar(100),

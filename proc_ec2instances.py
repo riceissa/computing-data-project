9bb9603e485e62ac5b10a0b0e4db87b9674eaac9
Issa Rice <riceissa@gmail.com>
2017-12-09 12:37:47 -0800

    Start on doing all the commits in a loop

--- proc_ec2instances.py
+++ proc_ec2instances.py
@@ -6,18 +6,27 @@
 # The idea is, we check out an appropriate version of the index.html
 # file, then run this script on it to extract the price info we want.
 
-# Commits to use:
-# 73514c58396c8e62944cd03380de387f69f29ca1 for 2017-08-16
-# 61ce503ddb12ec38c752e2830e4b51e7714c2a91 for 2017-05-19
-
 
 from bs4 import BeautifulSoup
 import re
 
 
-def main():
-    print("""insert into cloud_instance_costs(name, cost, odate, region, operating_system) values""")
+# These are chosen to be about 3 months apart, starting in September
+# 2017, which itself is 3 months prior to December 2017 (when this
+# project began)
+commits = [
+    "73514c58396c8e62944cd03380de387f69f29ca1",
+    "61ce503ddb12ec38c752e2830e4b51e7714c2a91",
+]
 
+
+def main():
+    for commit in commits:
+        print_sql(commits)
+
+
+def print_sql(commit):
+    """Print the SQL insert lines for the given commit hash."""
 
     # The default region seems to be Virginia and that's what we want
     # anyway, so just hard-code that
@@ -30,13 +39,19 @@ def main():
     m = re.match('Last [Uu]pdate: (\d\d\d\d-\d\d-\d\d) .*$', lu[0])
     last_update = m.group(1)
 
+    print("""# Prices from commit {}""".format(commit))
+
+    print("""insert into cloud_instance_costs(name, cost, odate, region, operating_system) values""")
+
     table = soup.find("table")
     first = True
     for tr in table.find_all("tr"):
         apiname = tr.find("td", {"class": "apiname"})
         cost = tr.find("td", {"class": "cost-ondemand-linux"})
         if apiname and cost:
-            cost = cost.text.strip()[len("$"):-len(" hourly")]
+            cost = cost.text.strip()
+            assert cost.startswith("$") and cost.endswith(" hourly")
+            cost = cost[len("$"):-len(" hourly")]
             print("    " + ("" if first else ",") +
                   """('{}',{},'{}','{}','{}')""".format(
                       apiname.text.strip(),

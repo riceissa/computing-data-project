62f99cccc0d477838e7eaeb3830d0efe21cd82d1
Issa Rice <riceissa@gmail.com>
2017-12-09 12:02:35 -0800

    Print the actual insert lines

--- proc_ec2instances.py
+++ proc_ec2instances.py
@@ -11,21 +11,37 @@ import re
 
 
 def main():
+    print("""insert into cloud_instance_costs(name, cost, odate, region, operating_system) values""")
+
+
+    # The default region seems to be Virginia and that's what we want
+    # anyway, so just hard-code that
+    region = "US East (N. Virginia)"
+
     with open("../ec2instances.info/www/index.html", "r") as f:
         soup = BeautifulSoup(f, "lxml")
 
-    update_date = soup.body.find_all(text=re.compile(r'Last [Uu]pdate:.*$'))
-    m = re.match('Last [Uu]pdate: (\d\d\d\d-\d\d-\d\d) .*$', update_date[0])
-    if m:
-        print(m.group(1))
+    lu = soup.body.find_all(text=re.compile(r'Last [Uu]pdate:.*$'))
+    m = re.match('Last [Uu]pdate: (\d\d\d\d-\d\d-\d\d) .*$', lu[0])
+    last_update = m.group(1)
 
     table = soup.find("table")
+    first = True
     for tr in table.find_all("tr"):
         apiname = tr.find("td", {"class": "apiname"})
         cost = tr.find("td", {"class": "cost-ondemand-linux"})
         if apiname and cost:
             pass
             # print(apiname.text.strip(), cost.text.strip())
+            print("    " + ("" if first else ",") +
+                  """('{}',{},'{}','{}','{},)""".format(
+                      apiname.text.strip(),
+                      cost.text.strip(),
+                      last_update,
+                      region,
+                      'Linux'
+                  ))
+            first = False
 
 if __name__ == "__main__":
     main()

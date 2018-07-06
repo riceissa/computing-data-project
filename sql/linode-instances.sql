5d057823c71a3e742d988b3994337deb95f13fd6
Issa Rice <riceissa@gmail.com>
2018-07-06 16:03:09 -0700

    Add Linode data in new format

--- sql/linode-instances.sql
+++ sql/linode-instances.sql
@@ -1,13 +1,13 @@
-# Pricing data from http://archive.today/2018.07.06-175634/https://www.linode.com/pricing
-insert into cloud_instance_costs(name, cost, odate, region, operating_system) values
-    ('Nanode 1GB',0.0075,'2018-07-06',NULL,NULL)
-    ,('Linode 2GB',0.015,'2018-07-06',NULL,NULL)
-    ,('Linode 4GB',0.03,'2018-07-06',NULL,NULL)
-    ,('Linode 8GB',0.06,'2018-07-06',NULL,NULL)
-    ,('Linode 16GB',0.12,'2018-07-06',NULL,NULL)
-    ,('Linode 24GB',0.09,'2018-07-06',NULL,NULL)
-    ,('Linode 48GB',0.18,'2018-07-06',NULL,NULL)
-    ,('Linode 90GB',0.36,'2018-07-06',NULL,NULL)
-    ,('Linode 150GB',0.72,'2018-07-06',NULL,NULL)
-    ,('Linode 300GB',1.44,'2018-07-06',NULL,NULL)
+# Data from http://archive.today/2018.07.06-175634/https://www.linode.com/pricing
+insert into cloud_instances(service,date_observed,name,ram,cpu,storage_type,storage_size,cost) values
+    ('Linode','2018-07-06','Nanode 1GB',1,1,'SSD',25,0.0075)
+    ,('Linode','2018-07-06','Linode 2GB',2,1,'SSD',50,0.015)
+    ,('Linode','2018-07-06','Linode 4GB',4,2,'SSD',80,0.03)
+    ,('Linode','2018-07-06','Linode 8GB',8,4,'SSD',160,0.06)
+    ,('Linode','2018-07-06','Linode 16GB',16,6,'SSD',320,0.12)
+    ,('Linode','2018-07-06','Linode 24GB',24,1,'SSD',20,0.09)
+    ,('Linode','2018-07-06','Linode 48GB',48,2,'SSD',40,0.18)
+    ,('Linode','2018-07-06','Linode 90GB',90,4,'SSD',90,0.36)
+    ,('Linode','2018-07-06','Linode 150GB',150,8,'SSD',200,0.72)
+    ,('Linode','2018-07-06','Linode 300GB',300,16,'SSD',340,1.44)
 ;

2f55844995e1b5698d35263e5da732e30fd0649e
Issa Rice <riceissa@gmail.com>
2018-07-07 12:36:48 -0700

    Add index

---
+++ access-portal/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include_once("backend/globalVariables/passwordFile.inc"); ?>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <meta name="dcterms.date" content="2018-07-07">
+    <meta property="og:title" content="Hello World" />
+    <meta property="og:site_name" content="Computing Data Project" />
+    <meta property="og:locale" content="en_US" />
+    <title>Hello World - Computing Data Project</title>
+    <link rel="stylesheet" href="/tablesorter.css">
+    <script src="/jquery.min.js"></script>
+    <script src="/jquery.tablesorter.js"></script>
+    <script src="/anchor.min.js"></script>
+  </head>
+  <body>
+    <?php include("backend/list_instances.inc"); ?>
+    <script>
+        $(function(){$("table").tablesorter();});
+        anchors.add();
+    </script>
+  </body>
+</html>

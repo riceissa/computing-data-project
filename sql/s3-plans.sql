aa09c231f621952e67cc9cc6795152814190a9a8
Issa Rice <riceissa@gmail.com>
2018-07-15 18:30:33 -0700

    start adding s3 data

---
+++ sql/s3-plans.sql
@@ -0,0 +1,3 @@
+insert into storage_plans(provider, name, date_observed, region, storage_cost, download_cost, write_op_cost, read_op_cost, list_op_cost, delete_op_cost, minimum_duration) values
+    ('Amazon S3','S3 Standard Storage','2018-07-15','US East (N. Virginia)','[[50, 0.023], [500, 0.022], [null, 0.021]]',0,0.005,0.0004,0.005,0,0)
+;

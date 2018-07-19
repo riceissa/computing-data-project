ca4b1ccb98aec476f8619eab1a9bc3c1b041c803
Issa Rice <riceissa@gmail.com>
2018-07-18 17:17:20 -0700

    more azure

--- sql/azure-virtual-machines.sql
+++ sql/azure-virtual-machines.sql
@@ -1,3 +1,8 @@
-insert into cloud_instances(provider,name,date_observed,region,ram,cpu,cost,operating_system) values
-    ('Microsoft Azure','B1S','2018-07-18','East US',1,1,0.011,'Linux')
+insert into cloud_instances(provider,name,date_observed,region,cpu,ram,storage_size,cost,operating_system) values
+    ('Microsoft Azure','B1S','2018-07-18','East US',1,1,2,0.011,'Linux')
+    ,('Microsoft Azure','B2S','2018-07-18','East US',2,4,8,0.042,'Linux')
+    ,('Microsoft Azure','B1MS','2018-07-18','East US',1,2,4,0.021,'Linux')
+    ,('Microsoft Azure','B2MS','2018-07-18','East US',2,8,16,0.084,'Linux')
+    ,('Microsoft Azure','B4MS','2018-07-18','East US',4,16,32,0.167,'Linux')
+    ,('Microsoft Azure','B8MS','2018-07-18','East US',8,32,64,0.335,'Linux')
 ;

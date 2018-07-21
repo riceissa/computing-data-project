8d69f6bff1fd3ca266530e2cfee545ca5abc6680
Issa Rice <riceissa@gmail.com>
2018-07-21 10:31:43 -0700

    Add more comments

--- sql/network-transfer-schema.sql
+++ sql/network-transfer-schema.sql
@@ -7,10 +7,18 @@ create table network_transfer (
     # on which the data was entered
     date_observed date,
 
-    # This should be the same as the region
+    # For outgoing/egress this should be the same as the instance region
     source varchar(500),
 
     destination varchar(500),
 
+    # The transfer cost as a string. If the cost is just a single rate,
+    # the format is a float (as a string), in dollars per GB. Otherwise it is a
+    # tiered plan and this should be a JSON list of lists, where the inner list
+    # is [bound, rate] for each tier (bound in TB, rate in dollars per GB).
+    # The bound on the final tier item is not used (as it is the tier
+    # saying "everything above the previous tier"), and should be null.
+    # The tiers must be ordered by the bound.
+    # Example: '[[50, 0.023], [500, 0.022], [null, 0.021]]'.
     transfer_cost varchar(100)
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;

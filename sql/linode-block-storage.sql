6fa953e4b7df8da5fc36d4ab5e4ef29226507854
Issa Rice <riceissa@gmail.com>
2019-06-18 17:38:10 -0700

    Add new Linode block storage data

--- sql/linode-block-storage.sql
+++ sql/linode-block-storage.sql
@@ -1,3 +1,5 @@
+# Data from https://www.linode.com/blockstorage
 insert into storage_plans(provider,name,date_observed,region,storage_cost,storage_type,minimum_volume_size,maximum_volume_size) values
     ('Linode','Linode Block Storage','2018-07-18','Newark','0.10','HDDs and NVMe drives',10.7,1100)
+    ,('Linode','Linode Block Storage','2019-06-18','Newark','0.10','HDDs and NVMe drives',10.7,1100)
 ;

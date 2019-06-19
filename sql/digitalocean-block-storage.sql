933b0c0614c27e3c48eb6b176ba9cab99c98665e
Issa Rice <riceissa@gmail.com>
2019-06-18 17:48:46 -0700

    Add DigitalOcean block storage data

--- sql/digitalocean-block-storage.sql
+++ sql/digitalocean-block-storage.sql
@@ -1,3 +1,4 @@
 insert into storage_plans(provider,name,date_observed,storage_cost,storage_type) values
     ('DigitalOcean','Block Storage','2018-07-18','0.10','SSD')
+    ,('DigitalOcean','Block Storage','2019-06-18','0.10','SSD')
 ;
